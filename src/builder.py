c037a30d5fc492fd6418ebf5efde53c384d2c516
Hoesu <hoesu.chung@qraftec.com>
2025-11-26 11:30:02 +0900

    FEAT: builder pseudo code.

---
+++ src/builder.py
@@ -0,0 +1,189 @@
+import logging
+import numpy as np
+import pandas as pd
+import networkx as nx
+
+from src.database import Base, execute_query
+from src.metric import compute_pairwise_dist
+from src.preprocess import allocate_properties, impute_data
+
+logger = logging.getLogger('project.builder')
+
+
+class DataProtectionPipeline:
+
+    def __init__(self, config: dict):
+        self.config = config
+        
+    def _insert_data(self):
+        engine = Base.get_engine()
+        Base.metadata.create_all(engine)
+        self.inserted_rows = execute_query(engine, self.config)
+
+    def _prepare_data(self):
+        engine = Base.get_engine()
+        Base.metadata.create_all(engine)
+        raw_data = execute_query(engine, self.config)
+        self.properties = allocate_properties(raw_data, self.config)        
+        self.data = impute_data(raw_data, self.properties, self.config)
+
+    def _compute_edge_weight(
+        self,
+        distance: float,
+        sigma: float = 1.0,
+    ) -> float:
+        """
+        거리 값을 기반으로 엣지 가중치를 계산합니다.
+
+        가중치는 거리의 역변환 함수를 통해 계산됩니다.
+        w = exp(-d/σ), 여기서 d는 거리, σ는 스케일 파라미터
+
+        Parameters
+        ----------
+        distance : float
+            두 노드 간의 거리
+        sigma : float, optional
+            스케일 파라미터 (기본값: 1.0)
+
+        Returns
+        -------
+        float
+            계산된 엣지 가중치
+        """
+        # TODO: distance가 음수가 아닌지 검증
+        # TODO: sigma가 양수인지 검증
+        # TODO: 가중치 공식 적용: w = exp(-d/σ)
+        # TODO: 가중치 값 반환
+        pass
+
+    def _build_graph_from_distances(
+        self,
+        distance_matrix: np.ndarray,
+        k: int,
+        sigma: float = 1.0,
+    ) -> nx.Graph:
+        """
+        거리 행렬을 기반으로 K-Nearest Neighbors 그래프를 구축합니다.
+
+        Parameters
+        ----------
+        distance_matrix : np.ndarray
+            모든 노드 쌍에 대한 거리 행렬 (n x n 형태)
+        k : int
+            각 노드에 연결할 최근접 이웃의 개수
+        sigma : float, optional
+            엣지 가중치 계산에 사용되는 스케일 파라미터 (기본값: 1.0)
+
+        Returns
+        -------
+        nx.Graph
+            구축된 NetworkX 그래프 객체
+        """
+        # TODO: distance_matrix의 shape 검증 (정사각 행렬인지 확인)
+        # TODO: k 값이 노드 개수보다 작은지 검증
+        # TODO: NetworkX 그래프 객체 생성 (nx.Graph())
+        # TODO: 모든 노드 추가 (0부터 n-1까지)
+        # TODO: 각 노드 i에 대해:
+        #   - distance_matrix[i]에서 자기 자신(i)을 제외한 거리 배열 추출
+        #   - 거리 기준으로 정렬하여 상위 k개의 인덱스 선택
+        #   - 선택된 각 이웃 노드 j에 대해:
+        #     * 엣지 (i, j)가 이미 존재하는지 확인 (중복 방지)
+        #     * distance_matrix[i, j]를 사용하여 가중치 계산 (_compute_edge_weight 호출)
+        #     * 그래프에 엣지 추가 (weight 속성 포함)
+        # TODO: 구축된 그래프 반환
+        pass
+
+    def _build_graph(self, data: pd.DataFrame, properties: dict) -> nx.Graph:
+        """
+        데이터프레임을 기반으로 그래프 네트워크를 구축합니다.
+
+        Parameters
+        ----------
+        data : pd.DataFrame
+            그래프를 구축할 데이터프레임
+        properties : dict
+            컬럼별 타입과 메트릭 정보를 담은 딕셔너리
+
+        Returns
+        -------
+        nx.Graph
+            구축된 NetworkX 그래프 객체
+        """
+        # TODO: config에서 필요한 파라미터 추출
+        #   - k: 최근접 이웃 개수 (기본값 설정 고려)
+        #   - sigma: 스케일 파라미터 (기본값: 1.0)
+        # TODO: compute_pairwise_dist(data, properties) 호출하여 거리 행렬 계산
+        # TODO: _build_graph_from_distances 함수 호출하여 그래프 구축
+        # TODO: 구축된 그래프 반환
+        pass
+
+    def _calculate_risk(self, graph: nx.Graph, data: pd.DataFrame) -> dict:
+        """
+        그래프 네트워크로부터 노출 위험도를 계산합니다.
+
+        Parameters
+        ----------
+        graph : nx.Graph
+            구축된 그래프 네트워크
+        data : pd.DataFrame
+            원본 데이터프레임
+
+        Returns
+        -------
+        dict
+            노출 위험도 정보를 담은 딕셔너리
+        """
+        # TODO: calculate_risk 함수 import 확인
+        # TODO: calculate_risk(graph, data, self.experiment_dict, self.config) 호출
+        # TODO: 계산된 위험도 반환
+        pass
+
+    def _save_results(
+        self,
+        data: pd.DataFrame,
+        graph: nx.Graph,
+        disclosure_risk: dict,
+    ) -> None:
+        """
+        결과물을 저장합니다.
+
+        Parameters
+        ----------
+        data : pd.DataFrame
+            전처리된 데이터
+        graph : nx.Graph
+            구축된 그래프
+        disclosure_risk : dict
+            계산된 노출 위험도
+        """
+        # TODO: save_result 함수 import 확인
+        # TODO: save_result(data, self.experiment_dict, graph, disclosure_risk, self.config) 호출
+        pass
+
+    def run(self) -> None:
+        
+        logger.info('Pipeline started')
+        
+        if self.config['mode'] == 'data':
+            self._insert_data()
+            logger.info(f'Data inserted: {self.inserted_rows} rows')
+            logger.info('Pipeline completed (data insertion mode)')
+            return
+        
+        self._prepare_data()
+        logger.info(f'Data shape: {self.data.shape}')
+        logger.info(f'Properties: {self.properties}')
+        
+        # 그래프 구축
+        # TODO: graph = self._build_graph(self.data, self.properties) 호출
+        # TODO: logger.info로 그래프 구축 완료 로그 출력
+        
+        # 위험도 계산
+        # TODO: disclosure_risk = self._calculate_risk(graph, self.data) 호출
+        # TODO: logger.info로 위험도 계산 완료 로그 출력
+        
+        # 결과 저장
+        # TODO: self._save_results(self.data, graph, disclosure_risk) 호출
+        # TODO: logger.info로 결과 저장 완료 로그 출력
+        
+        logger.info('Pipeline completed')
--- src/graph/__init__.py
+++
@@ -1,4 +0,0 @@
-from .builder import *
-from .faiss_wrapper import *
-
-__all__ = []
